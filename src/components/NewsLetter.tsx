3077154f62b40e0c8560c467f7c1fb6f03c58f18
wapmastaz <demolatheophilus8@gmail.com>
2025-12-16 21:34:28 +0100

    designed the newsletter button

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -12,6 +12,7 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
 import {Check} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
 
 const FormSchema = z.object({
   email: z.string().check(z.email('Please enter a valid email address.')),
@@ -40,19 +41,25 @@ export default function NewsLetter() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-80 space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-80 space-y-4">
         <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input placeholder="Email address" {...field} className="placeholder:text-white" />
+                <Input
+                  {...field}
+                  placeholder="Email address"
+                  className="placeholder:text-gray-600 h-12 text-gray-700 bg-white rounded-[420px]" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+        <Button className="font-bold font-poppins text-lg w-full bg-[#FF005C] h-12 rounded-[42px]">
+          Subscribe
+        </Button>
       </form>
     </Form>
   );
